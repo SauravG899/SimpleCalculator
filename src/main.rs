756b30b230f021f261d9718a5a11df7d7a337810
Saurav Guduru <67701137+SauravG899@users.noreply.github.com>
2021-01-03 21:53:23 -0500

    added option to choose function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod stats_calculator;
+mod quadratic;
+mod calculator;
+
+use std::io;
 
 fn main(){
-    stats_calculator::run();
+    
+    println!("Which function would you like to use? (Stats (a), General (b), Quadratic (c) )");
+    
+    let mut _typeofcalc_buffer = String::new();
+    
+    io::stdin().read_line(& mut _typeofcalc_buffer).unwrap();
 
+    let choiceoffunc = _typeofcalc_buffer.trim_end();
+
+    match choiceoffunc {
+        "a" => stats_calculator::run(),
+        "b" => calculator::input(),
+        "c" => todo!("Coming Soon!"),
+        _ => panic!(),
+    };
 }
